123dbea64f7d7b9dcc08b3006eddd54986a2cf5d
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-27 16:33:38 -0700

    add employee

--- employeeDB.sql
+++ employeeDB.sql
@@ -32,6 +32,10 @@ VALUES ("Sales"), ("Creative"), ("Executive");
 INSERT INTO role (title, salary, department_id)
 VALUES ("Creative Director", 250000.00, 2), ("Head of Sales", 210000.00, 1), ("President", 350000.00, 3);
 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 120000, 1), ("Sales Person", 80000, 1),
+("Lead Copywriter", 80000, 2), ("Copywriter", 55000, 2);
+
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Bert", "Cooper", 3);
 
@@ -50,7 +54,10 @@ SELECT title, salary, name
 FROM role INNER JOIN department
 ON role.department_id = department.id; 
 
-
+-- View Departments
+SELECT name, title
+FROM department INNER JOIN role
+ON department.id = role.department_id;
 
 SELECT title, salary, name
 FROM role INNER JOIN department
--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function init() {
           "View All Employees",
           "View All Roles",
           "View All Departments",
+          "Add An Employee",
           "Exit",
         ],
       },
@@ -45,7 +46,11 @@ function init() {
           break;
         case "View All Departments":
           viewDepartments();
+        case "Add An Employee":
+          addEmployee();
         default:
+          // @ ToDo fix end function
+          // Error: Cannot enqueue Quit after invoking quit.
           connection.end();
       }
     });
@@ -71,4 +76,63 @@ function viewRoles() {
       init();
     }
   );
+}
+
+function viewDepartments() {
+  connection.query(
+    "SELECT name, title FROM department INNER JOIN role ON department.id = role.department_id;",
+    function (err, data) {
+      console.table(data);
+      init();
+    }
+  );
+}
+
+// ----------INSERT FUNCTIONS--------------------
+
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "Enter The Employees First Name: ",
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "Enter The Employees Last Name: ",
+    },
+    {
+      type: "list",
+      name: "title",
+      message: "Enter The Employees Role",
+      choices: [
+          "Sales Lead",
+          "Sales Person",
+          "Lead Copywriter",
+          "Copywriter",
+      ]
+    },
+   
+  ]).then(function(response){
+      console.log(response);
+      const query = "INSERT INTO employee (first_name, last_name, role_id, manager_id)";
+
+       
+        let roleID;
+        let manager;
+        switch(response.title){
+            case "Sales Lead":
+                return roleID = 4, manager = 3;
+            case "Sales Person":
+                return roleID = 5;
+            case "Lead Copywriter":
+                return roleID = 6, manager = 2;
+        }
+        const employee = connection.query(query, [response.first_name, response.last_name, roleID, manager], function(err, data){
+            console.log(`Added Employee ${response.first_name} ${response.last_name} as a ${response.title}`);
+            init();
+        })
+      
+  })
 }
