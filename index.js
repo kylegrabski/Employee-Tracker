3f482d472c88bb3f1617448d9f299c42d32ace5d
Kyle Grabski <kyle.grabski@gmail.com>
2021-03-28 14:41:01 -0700

    functional addEmployee/Role

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const table = cTable;
+
+// The Manager ID is equal to the employee ID. So The manager id of 2 means the 
+// employees manager is the employee with the employee ID of 2
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -20,6 +24,7 @@ connection.connect((err) => {
   init();
 });
 
+// @ToDo create ASCII art on load up
 function init() {
   inquirer
     .prompt([
@@ -32,6 +37,7 @@ function init() {
           "View All Roles",
           "View All Departments",
           "Add An Employee",
+          "Add A Role",
           "Exit",
         ],
       },
@@ -46,9 +52,17 @@ function init() {
           break;
         case "View All Departments":
           viewDepartments();
+          break;
         case "Add An Employee":
           addEmployee();
-        default:
+          break;
+        case "Add A Role":
+          addRole();
+          break;
+        case "Add A Department":
+          addDepartment();
+          break;
+        case "Exit":
           // @ ToDo fix end function
           // Error: Cannot enqueue Quit after invoking quit.
           connection.end();
@@ -60,7 +74,7 @@ function init() {
 
 function viewEmployees() {
   connection.query(
-    "SELECT employee.first_name, employee.last_name, role.title FROM employee INNER JOIN role ON employee.role_id = role.id;",
+    "SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id;",
     function (err, data) {
       console.table(data);
       init();
@@ -80,7 +94,8 @@ function viewRoles() {
 
 function viewDepartments() {
   connection.query(
-    "SELECT name, title FROM department INNER JOIN role ON department.id = role.department_id;",
+    // "SELECT name, title FROM department INNER JOIN role ON department.id = role.department_id;",
+    "SELECT * FROM department;",
     function (err, data) {
       console.table(data);
       init();
@@ -91,48 +106,149 @@ function viewDepartments() {
 // ----------INSERT FUNCTIONS--------------------
 
 function addEmployee() {
+    // will update the Employees Role whenever a new Role is created
+    let titlesDB = [];
+    connection.query("SELECT * FROM role", function(err, data){
+            if (err) console.log(err, " FROM SELECTING ROLES IN addEmployee");
+            for (let i = 0; i < data.length; i++) {
+                titlesDB.push(data[i].title)
+                
+            }
+    })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Enter The Employees First Name: ",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Enter The Employees Last Name: ",
+      },
+      {
+        type: "list",
+        name: "title",
+        message: "Enter The Employees Role",
+        choices: titlesDB
+      },
+    ])
+    .then(function (response) {
+      console.log(response);
+      const query =
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);";
+
+        // @ ToDo create For Loop to assign roleID and manager
+        // based on what user chooses instead of hard coding. WONT SCALE
+      let roleID = 0;
+      let manager = 0;
+    //   switch (response.title) {
+    //     case "Sales Lead":
+    //       return roleID = 4, manager = 3;
+          
+    //     //   break;
+    //     case "Sales Person":
+    //       return roleID = 5, manager = 3;
+          
+    //     //   break;
+    //     case "Lead Copywriter":
+    //       return roleID = 6, manager = 2;
+         
+    //     //   break;
+    //     case "Copy Writer":
+    //         return roleID = 7, manager = 2;            
+    //    }
+    if (response.title === "Sales Lead"){
+        roleID = 4
+        manager = 3
+    }else if(response.title === "Sales Person"){
+        roleID = 5
+        manager = 3
+    }else if(response.title === "Lead Copywriter"){
+        roleID = 6
+        manager = 2
+    }else if(response.title === "Copywriter"){
+        roleID = 7
+        manager = 2
+    }
+      const employee = connection.query(query, [response.firstName, response.lastName, roleID, manager],
+        function (err, data) {
+          console.log(
+            "Added Employee",
+            response.firstName,
+            response.lastName,
+            "for the position of",
+            response.title
+          );
+          init();
+        }
+      );
+    });
+}
+
+function addRole(){
+  // will update the department_id whenever a new Department is created
+  let departmentDB = [];
+  connection.query("SELECT * FROM department", function(err, data){
+    if (err) console.log(err, " FROM SELECTING DEPARTMENT IN add Role");
+    for (let i = 0; i < data.length; i++) {
+      departmentDB.push(data[i].name)
+      
+    }
+  })
   inquirer.prompt([
     {
       type: "input",
-      name: "firstName",
-      message: "Enter The Employees First Name: ",
+      name: "title",
+      message: "Enter The Title Of The Role: "
     },
     {
       type: "input",
-      name: "lastName",
-      message: "Enter The Employees Last Name: ",
+      name: "salary",
+      message: "Enter The Salary For The Role"
     },
     {
       type: "list",
-      name: "title",
-      message: "Enter The Employees Role",
-      choices: [
-          "Sales Lead",
-          "Sales Person",
-          "Lead Copywriter",
-          "Copywriter",
-      ]
-    },
-   
-  ]).then(function(response){
-      console.log(response);
-      const query = "INSERT INTO employee (first_name, last_name, role_id, manager_id)";
+      name: "department",
+      message: "To What Department Does The Role Belong?: ",
+      choices: departmentDB
+    }
+  ])
+  .then(function (response){
+    console.log(response);
+    const query = 
+    "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);";
 
-       
-        let roleID;
-        let manager;
-        switch(response.title){
-            case "Sales Lead":
-                return roleID = 4, manager = 3;
-            case "Sales Person":
-                return roleID = 5;
-            case "Lead Copywriter":
-                return roleID = 6, manager = 2;
-        }
-        const employee = connection.query(query, [response.first_name, response.last_name, roleID, manager], function(err, data){
-            console.log(`Added Employee ${response.first_name} ${response.last_name} as a ${response.title}`);
-            init();
-        })
-      
+    // converting the salaray response into an INT to be able to
+    // insert into MySQL database
+    let salary = parseInt(response.salary);
+
+    // @ ToDo create For Loop to assign department_ID based on what user chooses instead of hardcoding. WONT SCALE.
+    let department_id = 0;
+    if(response.department === "Sales"){
+      department_id = 1
+    } else if (response.department === "Creative"){
+      department_id = 2
+    }else if (response.department === "Executive"){
+      department_id = 3
+    }
+
+    const role = connection.query(query, [response.title, salary, department_id], function (err, data){
+      if (err) console.log(err, "ERROR INSERTING NEW ROLE IN role TABLE")
+      console.log("Added Role", response.title, "In The", response.department, "Department", "With A Salary Of:", salary);
+      init();
+    })
   })
+}
+
+function addDepartment(){
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What Is The Name Of The Department You Want To Add?: "
+    }
+  ])
+  
 }
